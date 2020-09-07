4510e1a72339ee791d55787b284723190fe04449
蔡序强 <1781088743@qq.com>
2020-09-07 15:07:27 +0800

    springcloud config 配置中心

---
+++ cloud2020/cloud-config-center-3344/src/main/java/com/caixiaobai/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.caixiaobai.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+/**
+ * @作者: caixiaobai
+ * @时间：2020/09/04
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
