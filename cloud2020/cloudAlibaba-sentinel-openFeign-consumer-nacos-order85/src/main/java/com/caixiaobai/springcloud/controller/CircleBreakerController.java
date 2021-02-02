69280b10730994b01079bdabd0f9f4dd8c8dbafe
蔡序强 <1781088743@qq.com>
2021-02-02 14:29:00 +0800

    2021.2.2

--- cloud2020/cloudAlibaba-sentinel-openFeign-consumer-nacos-order85/src/main/java/com/caixiaobai/springcloud/controller/CircleBreakerController.java
+++ cloud2020/cloudAlibaba-sentinel-openFeign-consumer-nacos-order85/src/main/java/com/caixiaobai/springcloud/controller/CircleBreakerController.java
@@ -1,14 +1,16 @@
 package com.caixiaobai.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.caixiaobai.springcloud.entities.CommonResult;
 import com.caixiaobai.springcloud.entities.Payment;
+
+import com.caixiaobai.springcloud.sentinelExcetipnClass.myfallback;
 import com.caixiaobai.springcloud.service.PaymentService1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 
 /**
  * created with Intellij IDEA
@@ -22,10 +24,18 @@ import javax.annotation.Resource;
 public class CircleBreakerController {
 
     @Autowired
-    private PaymentService1 paymentService;
+    private PaymentService1 paymentService1;
 
     @RequestMapping(value = "/consumer/paymentSQL/{id}")
+    //@SentinelResource(value = "fallback",fallback = "demo2",fallbackClass = myfallback.class)//fallback负责业务异常返回结果,限流异常正常返回error page
+    //@SentinelResource(value = "fallback",blockHandler = "demo1",blockHandlerClass = myfallback.class)//负责sentinel限流异常返回结果,业务异常正常返回error page
+    @SentinelResource(value = "fallback",
+            fallback = "demo2",fallbackClass = myfallback.class,
+            blockHandler = "demo1",blockHandlerClass = myfallback.class)
+            //如果fallback和blockhandler都配置了的情况，当正常访问没有超过sentinel设置的相应规则，就正常返回查询结果，如果超过了就返回blockHandler配置的返回
+            //如果没有超过sentinel设置的规则，提供者内部出现了问题，就返回fallback配置的返回方法
+            //如果提供者内部出现了问题，在没有超过sentinel设置的规则的时候返回fallback配置的方法。如果超过了就返回blockHandler配置的方法
     public CommonResult<Payment> paymentSQL(@PathVariable("id")Long id){
-        return paymentService.paymentSQL(id);
+        return paymentService1.paymentSQL(id);
     }
 }
